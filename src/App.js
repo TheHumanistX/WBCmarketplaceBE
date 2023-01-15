6ce2cecd70eb5031bddcd81da207702379a6a199
TheHumanistX <thehumanistx@gmail.com>
2023-01-15 11:58:14 -0500

    New commit after beginning work on frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route, Routes}  from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <div className = "logo">
+            <h1><a href ="#">GrandPa's Toolbox</a></h1>
+          </div>
+
+          <ul>
+            <li><a href="./">Home</a></li>
+            <li><a href="/products">Products</a></li>
+            <li className = "nav-cta"><a href = "#">Connect</a></li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
