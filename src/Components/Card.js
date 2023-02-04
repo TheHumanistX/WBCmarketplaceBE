5b6ab16eee8d0af355c101944e01ac1d2700518a
Brian H (Tirwander) <brianfhancock@gmail.com>
2023-02-04 13:28:22 -0500

    push at end of project, but will be making more changes

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,9 +1,14 @@
 import css from './Card.css';
-import {ethers} from 'ethers';
+import { ethers, providers } from 'ethers';
 import ABI from './ABI.json';
 import tokenABI from './tokenABI.json';
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
+/**
+ * 
+ * @param {*} props 
+ * @returns 
+ */
 function Card(props) {
     const [Bought, setBought] = useState(false);
 
@@ -11,26 +16,63 @@ function Card(props) {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const currentAddress = await provider.getSigner().getAddress();
-        const marketplaceContract = new ethers.Contract("0xA8D534cA3Fe692389515c0849C4cDcdDeaeAFA16", ABI, signer);
+        const marketplaceContract = new ethers.Contract("0x5737a7cF29081C35dc33Fb555E3E95B6f1Ffade2", ABI, signer);
         const boughtYet = await marketplaceContract.hasPurchased(currentAddress);
         setBought(boughtYet);
         console.log(Bought);
     }
 
-    const Connect = async() => {
+
+    const Connect = async () => {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         await provider.send("eth_requestAccounts");
         console.log("Trying to connect...");
     }
-    
-    const payInUSDC = async() => {
+
+    // const setupWalletEmpty = {
+    //     provider: new ethers.providers.Web3Provider(window.ethereum),
+    //     signer: setupObject.provider.getSigner(),
+    //     currentAddress: setupObject.provider.getSigner().getAddress(),
+
+    // };
+
+    const payInETH = async () => {
         Connect();
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         const currentAddress = await provider.getSigner().getAddress();
-        const marketplaceAddress = "0xA8D534cA3Fe692389515c0849C4cDcdDeaeAFA16";
+        const marketplaceAddress = "0x5737a7cF29081C35dc33Fb555E3E95B6f1Ffade2";
         const marketplaceContract = new ethers.Contract(marketplaceAddress, ABI, signer);
-        const usdcToken = new ethers.Contract("0xedF07e8dec965BF01a64B3Ab7Fe75aCa0F1a66f4", tokenABI, signer);
+        const amount = await provider.getBalance(currentAddress);
+        const formattedAmount = ethers.utils.formatEther(amount);
+        console.log(formattedAmount);
+        const price = await marketplaceContract.getPriceOfETH();
+        const formattedPrice = ethers.utils.formatEther(price);
+        console.log(price);
+        console.log(amount);
+
+        if (formattedAmount >= formattedPrice) {
+            // they can buy
+            const pay = await marketplaceContract.payInEth({ value: price });
+            console.log(pay);
+            const receipt = await pay.wait();
+            if (receipt.confirmations > 0) {
+                setBought(true);
+            }
+        } else {
+            // they can't buy
+        }
+    }
+
+
+    const payInUSDC = async () => {
+        Connect();
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const currentAddress = await provider.getSigner().getAddress();
+        const marketplaceAddress = "0x5737a7cF29081C35dc33Fb555E3E95B6f1Ffade2";
+        const marketplaceContract = new ethers.Contract(marketplaceAddress, ABI, signer);
+        const usdcToken = new ethers.Contract("0x86F96Bf18BBaA1fb5b0b1fE7CABC26B751dCe7F8", tokenABI, signer);
 
         const totalHolding = await usdcToken.balanceOf(currentAddress);
         const totalAllowance = await usdcToken.allowance(currentAddress, marketplaceAddress);
@@ -39,7 +81,7 @@ function Card(props) {
         console.log("Total Amount Held:" + totalHolding);
         console.log("Total Allowance:" + totalAllowance);
 
-        
+
         if (price <= totalHolding) {
             // they have enough to buy
             if (price <= totalAllowance) {
@@ -54,12 +96,12 @@ function Card(props) {
                     const purchase = await marketplaceContract.payInUSDC();
                     setBought(purchase);
                 }
-                
+
             }
         } else {
             // they don't have enough to buy
         }
-        
+
     }
 
     useEffect(() => {
@@ -83,22 +125,22 @@ function Card(props) {
                     <p class="text--medium">{props.description} </p>
 
                 </div>
-                
-                {Bought == true ? 
+
+                {Bought == true ?
                     <div>
-                        <p className = "card__price text__price">
-                            <a href = "/Item1">View Your Product</a>
+                        <p className="card__price text__price">
+                            <a href="/Item1">View Your Product</a>
                         </p>
                     </div>
                     :
                     <div>
                         <div>
-                            <img class="buyIcon" src="https://imgur.com/MQHRBrg.png" ></img>
-                            <img class="buyIcon" src="https://imgur.com/wndKTZS.png" ></img>
-                            <img class="buyIcon" src="https://imgur.com/sQsv7UD.png" ></img>
+                            <img onClick={payInUSDC} class="buyIcon" src="https://imgur.com/MQHRBrg.png" ></img>
+                            <img onClick={payInUSDC} class="buyIcon" src="https://imgur.com/wndKTZS.png" ></img>
+                            <img onClick={payInETH} class="buyIcon" src="https://imgur.com/sQsv7UD.png" ></img>
                         </div>
-                        
-                        <p onClick = {payInUSDC}>Check Buy</p>
+
+
                         <div>
                             <p class="card__price text__price">
                                 $10
